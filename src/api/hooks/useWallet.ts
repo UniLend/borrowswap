993859ccec8406d6b3cc187d40555f976573ee11
DAppArtisan <141224058+DAppArtisan@users.noreply.github.com>
2024-04-04 12:35:04 +0530

    unilend testnet network fix
    fix unilend test net network

--- src/api/hooks/useWallet.ts
+++ src/api/hooks/useWallet.ts
@@ -1,6 +1,6 @@
 import { useAccount } from 'wagmi';
 
 export default function useWalletHook() {
-  const { address, isConnected, chain, chainId, isReconnected } = useAccount();
-  return { address, isConnected, chain,chainId, isReconnected };
+  const { address, isConnected, chain, chainId } = useAccount();
+  return { address, isConnected, chain, chainId };
 }
