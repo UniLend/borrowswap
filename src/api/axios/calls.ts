a7fa9cd1023b32680c765cdfa9219d07d0e1054f
Anil Pujari <anilpujari@Anils-MacBook-Air.local>
2024-06-26 10:11:25 +0530

    unilend test

--- src/api/axios/calls.ts
+++ src/api/axios/calls.ts
@@ -13,7 +13,7 @@ export const fetchGraphQlData = async (chainId: number, FILMS_QUERY: any) => {
     1: "https://api.thegraph.com/subgraphs/name/shubham-rathod1/mainnet-1",
     42161:
       "https://api.thegraph.com/subgraphs/name/shubham-rathod1/unilend-arbritrum",
-    16715:
+    17970:
       "https://api.thegraph.com/subgraphs/name/shubham-rathod1/unilend-polygon",
   };
 
@@ -229,7 +229,7 @@ export const uniswapTokensData = async (chainId: number = 1) => {
     137: "https://tokens.coingecko.com/polygon-pos/all.json",
     56: "https://tokens.coingecko.com/binance-smart-chain/all.json",
     1285: "https://tokens.coingecko.com/moonriver/all.json",
-    16715: "https://tokens.coingecko.com/polygon-pos/all.json",
+    17970: "https://tokens.coingecko.com/polygon-pos/all.json",
   };
 
   const url = graphURL[chainId]; // Default to chainId 1 if not found
--- src/api/networks/networks.ts
+++ src/api/networks/networks.ts
@@ -198,8 +198,8 @@ export const supportedNetworks: any = {
     quoterContractAddress: "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6",
   },
 
-  16715: {
-    chainId: 16715,
+  17970: {
+    chainId: 17970,
     chainName: "unilendtest",
     nativeCurrency: {
       name: "token",
